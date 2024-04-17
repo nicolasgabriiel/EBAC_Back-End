9b6a438d6e2a169819ffd900bfe661859475eb5b
nicolasgabriiel_ <nicolasgabriel.developer@gmail.com>
2024-04-17 10:22:45 -0300

    Módulo 9, Casting

---
+++ md9/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        long numero = 27032003;
+        System.out.println(numero);
+        Long numeroWrapper = numero;
+        System.out.println(numeroWrapper);
+    }
+}
