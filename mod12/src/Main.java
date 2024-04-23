afb30b8c583b661d146bf0e215c02b149b5d9d3f
nicolasgabriiel_ <nicolasgabriel.developer@gmail.com>
2024-04-23 15:57:44 -0300

    Atualizando tarefa 2

--- mod12/src/Main.java
+++ mod12/src/Main.java
@@ -1,10 +1,19 @@
 import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-
+        ordenarLista();
+        separarPorSexo();
+    }
+    public static void ordenarLista() {
+        System.out.println(" ");
+        System.out.println("**** Vamos criar uma lista de nomes e depois organizá-los ****");
+        System.out.println(" ");
         Scanner s = new Scanner(System.in);
-        System.out.println("Digite nomes separados por vírgula:");
+        System.out.println("Digite os nomes separados por vírgula:");
         String resposta = s.nextLine();
         List<String> listaDeNomes = new ArrayList<String>();
         listaDeNomes.addAll(Arrays.asList(resposta.split(",")));
@@ -16,6 +25,52 @@ public class Main {
         Collections.sort(listaDeNomes);
         System.out.println("**** Lista ordenada: ****");
         System.out.println(listaDeNomes);
+    }
+
+
+    public static void separarPorSexo() {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> homens = new ArrayList<>();
+        List<String> mulheres = new ArrayList<>();
+        System.out.println(" ");
+        System.out.println("**** Agora vamos criar uma lista de nomes de homens e mulheres e depois organizalos em suas próprias listas ****");
+        System.out.println(" ");
+
+        System.out.println("Quantas pessoas deseja cadastrar?");
+        int quantidade = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < quantidade; i++) {
+            System.out.println("Digite o nome da pessoa:");
+            String nome = scanner.nextLine();
+
+            System.out.println("Digite o sexo da pessoa (M para masculino, F para feminino):");
+            char sexo = scanner.nextLine().toUpperCase().charAt(0);
+
+            if (sexo == 'M') {
+                homens.add(nome);
+            } else if (sexo == 'F') {
+                mulheres.add(nome);
+            } else {
+                System.out.println("Sexo inválido. Por favor, digite M ou F.");
+                i--; // Decrementar o contador para repetir a iteração
+            }
+        }
+        System.out.println(" ");
+        System.out.println("*** Lista de homens: ***");
+        System.out.println(" ");
+        for (String homem : homens) {
+            System.out.println(homem);
+        }
+        System.out.println(" ");
+        System.out.println("*** Lista de mulheres: ***");
+        System.out.println(" ");
+        for (String mulher : mulheres) {
+            System.out.println(mulher);
+        }
+
+        scanner.close();
     }
 
 }
